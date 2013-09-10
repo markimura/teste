26a89a62a2fd81f2031ed06b231941bae3fa51f6
Usuario <Usuario@L05M05>
2013-09-09 21:01:02 -0300

    commit 1

---
+++ src/aula/Aula.java
@@ -0,0 +1,12 @@
+package aula;
+
+public class Aula {
+
+    public static void main(String[] args) {
+        int x;
+        
+        System.out.println("hello world");
+                
+             
+    }
+}
